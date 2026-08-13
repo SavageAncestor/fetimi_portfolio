016c0f78eb5da62340916f5e429308863a873572
SavageAncestor <timilayemifetimi@gmail.com>
2026-08-13 23:00:40 +0100

    Set up portfolio design foundation

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    fontFamily: 'SpaceGrotesk',
+    scaffoldBackgroundColor: const Color(0xFF050507),
+  );
+}
